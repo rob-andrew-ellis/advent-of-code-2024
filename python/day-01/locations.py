b43734124fdef1d1c654e6b1bdc86d648b10e2e9
rob-andrew-ellis <robandrewellis@gmail.com>
2024-12-14 11:06:56 +0000

    Made part 1 even shorter

--- python/day-01/locations.py
+++ python/day-01/locations.py
@@ -1,9 +1,6 @@
-if __name__ == "__main__":
-    list1, list2 = map(
-        sorted,
-        map(list, zip(*[map(int, line.split()) for line in open("input.txt", "r")])),
-    )
-    print(
-        f"The total distance between the lists is {sum([abs(list2[i] - list1[i]) for i in range(len(list1))])}"
-    )
-    print(f"The similarity score is {sum([num*list2.count(num) for num in list1])}")
+list1, list2 = map(
+    sorted,
+    map(list, zip(*[map(int, line.split()) for line in open("input.txt", "r")])),
+)
+print(f"Part 1: {sum([abs(list2[i] - list1[i]) for i in range(len(list1))])}")
+print(f"Part 2: {sum([num*list2.count(num) for num in list1])}")
